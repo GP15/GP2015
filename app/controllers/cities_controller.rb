37f76ad83aa43f19ac88ae669edcbbc666ecfb7e
Hisyam <hisyam.mdnor@gmail.com>
2015-05-31 23:27:55 +0800

    city model with basic add/update/delete

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,51 @@
+class CitiesController < ApplicationController
+
+  before_action :set_city, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cities = City.all
+  end
+
+  def show
+  end
+
+  def new
+    @city = City.new
+  end
+
+  def edit
+  end
+
+  def create
+    @city = City.new(city_params)
+
+    if @city.save
+      redirect_to cities_path, notice: 'City was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @city.update(city_params)
+      redirect_to cities_path, notice: 'City was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @city.destroy
+    redirect_to cities_path, notice: 'City was successfully deleted.'
+  end
+
+  private
+
+    def set_city
+      @city = City.find(params[:id])
+    end
+
+    def city_params
+      params.require(:city).permit(:name)
+    end
+end
