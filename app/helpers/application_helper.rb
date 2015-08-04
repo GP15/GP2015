1d01a2168dfc7db145ecb62b7ae8a3bb049d3e73
Hisyam <hisyam.mdnor@gmail.com>
2015-08-04 23:04:03 +0800

    refactored page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,24 @@
 module ApplicationHelper
 
-  # Change the logo link if user is signed in
+  # Returns the webpage's full title on a per-page basis.
+  def full_title(page_title = '', options = {})
+    if options == 'admin'
+      base_title = "GeniusPass (Admin)"
+    else
+      base_title = "GeniusPass"
+    end
+
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  # Changes the logo link. If user is signed in, redirect to user dashboard.
+  # Otherwise clicking the logo will redirect to homepage.
   def logo_url
     user_signed_in? ? current_user : root_url
-  end
-
-  # TODO: Refactor these 2 methods.
-
-  # Returns the full title on a per-page basis
-  def full_title(page_title = '')
-    base_title = "GeniusPass"
-    if page_title.empty?
-      base_title
-    else
-      "#{page_title} | #{base_title}"
-    end
-  end
-
-  # Returns the full title for admin pages on a per-page basis
-  def admin_full_title(page_title = '')
-    base_title = "GeniusPass (Admin)"
-    if page_title.empty?
-      base_title
-    else
-      "#{page_title} | #{base_title}"
-    end
   end
 
 end
