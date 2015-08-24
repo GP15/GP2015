25d82cda22cfa124a56b55fee644efdce07fdfe5
Hisyam <hisyam.mdnor@gmail.com>
2015-08-24 23:11:07 +0800

    redesigned partners#index and use masonry layout

---
+++ app/assets/javascripts/partners.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  // Transform list of partners in partners#index into masonry layout.
+  var $grid = $('.partner-grid').isotope({
+    // options
+    itemSelector: '.partner-grid-item',
+    masonry: {
+      columnWidth: 300,
+      isFitWidth: true,
+      "gutter": 30
+    }
+  });
+
+  // Unloaded images can throw off masonry layout and cause item elements to overlap.
+  // imagesLoaded.js resolves this issue.
+  // Initialize Isotope, then trigger layout after each image loads:
+  $grid.imagesLoaded().progress(function() {
+    $grid.isotope('layout');
+  });
+
+});
