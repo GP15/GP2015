0b5d083d9eb147677cb55604fb5f78e80f5df92e
Hisyam <hisyam.mdnor@gmail.com>
2015-08-30 23:51:37 +0800

    show upcoming & past reservations in user dashboard using tabbed interface

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
 
   def show
     @children = current_user.children.sort_by_age_name
-    @reservations = current_user.reservations
-                      .includes(:child, schedule: [:partner, :klass, :city])
-                      .order('schedules.starts_at ASC, schedules.ends_at ASC')
+    @reservations = current_user.reservations.includes(:child, schedule: [:partner, :klass, :city])
+
+    @current_reservations = @reservations.upcoming.sort_by_datetime_asc
+    @past_reservations = @reservations.created_between(2.months.ago, Time.zone.now).sort_by_datetime_desc
   end
 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,13 @@ class Reservation < ActiveRecord::Base
   belongs_to :schedule
   belongs_to :user
 
+  scope :upcoming, -> { where('schedules.starts_at >= ?', Time.zone.now) }
+
+  scope :sort_by_datetime_asc,  -> { order('schedules.starts_at ASC,  schedules.ends_at ASC') }
+  scope :sort_by_datetime_desc, -> { order('schedules.starts_at DESC, schedules.ends_at DESC') }
+
+  def self.created_between(start_time, end_time)
+    where('schedules.starts_at >= ? AND schedules.starts_at < ?', start_time, end_time)
+  end
+
 end
