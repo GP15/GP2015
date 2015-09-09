a2fc58d469ab1142b6a0be0822bcfb7ff977a2b8
Hisyam <hisyam.mdnor@gmail.com>
2015-09-09 18:42:12 +0800

    deleting a schedule will delete all of its reservations

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,7 +4,7 @@ class Schedule < ActiveRecord::Base
   belongs_to :partner
   belongs_to :activity
   belongs_to :klass
-  has_many   :reservations
+  has_many   :reservations, dependent: :destroy
   has_many   :children, through: :reservations
 
   validates :quantity, presence: true,
