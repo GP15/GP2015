19e54a5833f48430a2a9cd96df31211e4bf8df34
Hisyam <hisyam.mdnor@gmail.com>
2015-09-09 22:50:22 +0800

    misc fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!
 
+  # GET /users/:id
   def show
     @children = current_user.children.sort_by_age_name
     @reservations = current_user.reservations.includes(:child, schedule: [:partner, :klass, :city])
