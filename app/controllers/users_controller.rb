77a060b996d435478c810496288568b146bd0be1
Hisyam <hisyam.mdnor@gmail.com>
2015-09-10 16:55:48 +0800

    show all past reservations in user dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @reservations = current_user.reservations.includes(:child, schedule: [:partner, :klass, :city])
 
     @current_reservations = @reservations.upcoming.sort_by_datetime_asc
-    @past_reservations = @reservations.created_between(2.months.ago, Time.zone.now).sort_by_datetime_desc
+    @past_reservations = @reservations.in_the_past.sort_by_datetime_desc
   end
 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,12 +7,13 @@ class Reservation < ActiveRecord::Base
   validates_presence_of :child_id
 
   scope :upcoming, -> { where('schedules.starts_at >= ?', Time.zone.now) }
+  scope :in_the_past, -> { where('schedules.starts_at < ?', Time.zone.now) }
 
   scope :sort_by_datetime_asc,  -> { order('schedules.starts_at ASC,  schedules.ends_at ASC') }
   scope :sort_by_datetime_desc, -> { order('schedules.starts_at DESC, schedules.ends_at DESC') }
 
-  def self.created_between(start_time, end_time)
-    where('schedules.starts_at >= ? AND schedules.starts_at < ?', start_time, end_time)
-  end
+  # def self.created_between(start_time, end_time)
+  #   where('schedules.starts_at >= ? AND schedules.starts_at < ?', start_time, end_time)
+  # end
 
 end
