d9996099028ad69b093bb364c214440a717e70da
Hisyam <hisyam.mdnor@gmail.com>
2015-09-10 16:44:06 +0800

    fixed user profile page - added validations for email & password confirmation - added page title - adjusted some page elements

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   has_many :reservations
   has_many :schedules, through: :reservations
 
+  validates_presence_of :email
+  validates_confirmation_of :password
+
 end
