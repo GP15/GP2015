60981b07c946c9a17b9deb732c21fb676affa915
Hisyam <hisyam.mdnor@gmail.com>
2015-09-11 16:51:58 +0800

    validates email uniqueness when editing admin/user profile

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,6 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable, :lockable
 
   validates_presence_of :email
+  validates_uniqueness_of :email
   validates_confirmation_of :password
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :omniauthable, :validatable
+
   devise :database_authenticatable, :registerable, :rememberable, :trackable
 
   has_many :children, dependent: :destroy
@@ -8,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :schedules, through: :reservations
 
   validates_presence_of :email
+  validates_uniqueness_of :email
   validates_confirmation_of :password
 
 end
