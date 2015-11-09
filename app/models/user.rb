255ed4b7ca4b89e6952b7b4cc836b34995f764cd
pinak1180 <pinak.0605@gmail.com>
2015-11-09 22:20:48 +0530

    promo code changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,30 @@
 class User < ActiveRecord::Base
 include Payment
-
+  ## Devise ##
   devise :database_authenticatable, :registerable, :rememberable, :trackable
 
+  ## Associations ##
   has_many :children, dependent: :destroy
   has_many :reservations
   has_many :schedules, through: :reservations
   has_many :subscriptions, dependent: :destroy
 
+  ## Validations ##
   validates_presence_of :email, :name, :location
   validates_uniqueness_of :email
   validates_confirmation_of :password
 
+  ## Callbacks ##
   after_create :create_customer
 
+  ## Scopes ##
+  scope :with_code, ->(code){ find_by(promo_code: code) }
+
+  ## Instance Methods ##
   def create_customer
     customer = Braintree::Customer.create(first_name: name, email: email)
     self.customer_id = customer.customer.id
+    self.promo_code = self.generate_promo_code
     self.save!
   end
 
@@ -30,5 +38,13 @@ include Payment
 
   def customer
     Braintree::Customer.find(customer_id)
+  end
+
+  def generate_promo_code
+    code = ''
+    begin
+      code = SecureRandom.urlsafe_base64(7)
+    end while User.with_code(code).nil?
+    code
   end
 end
