a1e0c187d06139dc1d31c1be9c2f2ddb07c01e11
Binit Kumar <binit.kumar.hbs@gmail.com>
2016-02-02 21:04:34 +0530

    Loadmore to public

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,7 +1,7 @@
 class PartnersController < ApplicationController
   layout 'admin', except: [:index, :show]
 
-  before_action :authenticate_admin!, except: [:index, :show]
+  before_action :authenticate_admin!, except: [:index, :show, :loadmore]
   before_action :set_partner, only: [:show, :edit, :update, :destroy]
 
   def index
