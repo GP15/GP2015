e8b066289db3ba940265a21584474c5c2058a9bd
Binit Kumar <binit.kumar.hbs@gmail.com>
2016-02-17 14:14:15 +0530

    template name update

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -4,7 +4,7 @@ class WelcomeMailer < BaseMandrillMailer
   def invite(user)
     subject = "Welcome to GeniusPass"
     merge_vars = {}
-    body = mandrill_template("welcome_to_maildrill", merge_vars)
+    body = mandrill_template("welcome_to_geniuspass", merge_vars)
 
     send_mail(user.email, subject, body)
   end
