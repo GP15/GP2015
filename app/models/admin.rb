3970208e50f2f4a9368c39119dd67b062cf225a3
Nicholas Ng <secret@live.com.my>
2016-07-14 15:46:05 +0800

    Deleting admin views

--- app/models/admin.rb
+++
@@ -1,9 +0,0 @@
-class Admin < ActiveRecord::Base
-
-  devise :database_authenticatable, :rememberable, :trackable, :lockable
-
-  validates_presence_of :email
-  validates_uniqueness_of :email
-  validates_confirmation_of :password
-
-end
