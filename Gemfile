3a7808efa3c8de204255f3d66e732dd70518ed3d
Nicholas Ng <secret@live.com.my>
2016-08-18 12:39:04 +0800

    Add delayedjob
    for stupid heroku

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'awesome_print'
 gem 'better_errors', '1.1.0'
 gem 'binding_of_caller', '0.7.2'
 
-gem 'delayed_job'
+gem 'delayed_job_active_record'
 
 group :doc do
   gem 'sdoc', '~> 0.4.0'            # bundle exec rake doc:rails generates the API under doc/api.
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Geniuspass
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_job.queue_adapter = :delayed_job
 
     # Tell Rails to use the fonts folder in /assets
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
